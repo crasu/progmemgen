bfdbfa9496507f18369ac06755bed428860b23d0
Christian Krämer <spam@crasu.de>
2023-10-23 23:23:24 +0200

    fix commandline parsing

--- lib/bin/generate.ts
+++ lib/bin/generate.ts
@@ -4,9 +4,9 @@ import { generateHeaderFile } from '../index.js'
 
 if (process.argv.length >= 4) {
 	const outputfile = process.argv.pop()
-	const inputfiles = process.argv
+	const inputfiles = process.argv.slice(2)
 	generateHeaderFile(inputfiles, outputfile)
-	console.log(`Inputfiles written to ${outputfile}`)
+	console.log(`Inputfiles ${inputfiles.join(",")} written to ${outputfile}`)
 } else {
 	console.log(
 		'USAGE: npx progmemgen [inputfile1.js inputfile.html 2 ...] outputfile.h'
